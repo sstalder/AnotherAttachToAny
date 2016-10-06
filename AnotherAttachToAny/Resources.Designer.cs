927de0eaf57723fe7fbcfacb15234d1c3903faa5
arcdev <arcticdev@gmail.com>
2016-10-06 10:58:23 -0400

    rename files to AnotherAttachToAny

--- AnotherAttachToAny/Resources.Designer.cs
+++ AnotherAttachToAny/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ArcDev.AttachToAny {
+namespace ArcDev.AnotherAttachToAny {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ArcDev.AttachToAny {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ArcDev.AttachToAny.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ArcDev.AnotherAttachToAny.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
